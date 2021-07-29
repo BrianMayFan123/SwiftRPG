7f5763557932ff0f5d538e4c19493ea45e5341bd
Conner Evans <conner.evans5705@gmail.com>
2021-07-29 22:38:50 +0100

    Initial Commit

---
+++ Swift RPG/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Swift RPG
+//
+//  Created by Conner Evans on 29/07/2021.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
